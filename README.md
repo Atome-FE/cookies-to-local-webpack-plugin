1e81cb0a8e350c2bdffa52cfcbfdc48449e7758f
baixiaojian <xiaojian.bai@advance.ai>
2021-07-05 21:32:28 +0800

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+# cookies-to-local-webpack-plugin
+
+Get the cookie under the domain name and write it to the local development environment
+
+## example
+
+```js
+// ...
+const cookiesToLocal = require('cookies-to-local-webpack-plugin')
+module.exports = {
+  webpack: override(
+    // ...
+    addWebpackPlugin(
+      new cookiesToLocal({
+        targetUrl: 'https://www.google.com',
+        localPort: 8020,
+        ars: ['--auto-open-devtools-for-tabs']
+      })
+    ),
+  ),
+  devServer: overrideDevServer(devProxy()),
+}
+
+```
---
+++ index.js
@@ -0,0 +1,45 @@
+const puppeteer = require('puppeteer');
+const chrome = require('chrome-cookies-secure');
+
+class AutoFillCookie {
+  constructor(options) {
+    this.options = options;
+  }
+
+  apply(compiler) {
+    const { targetUrl, localPort, args = [] } = this.options
+    compiler.hooks.done.tap('AutoFillCookie', () => {
+        chrome.getCookies(targetUrl, async (err, cookies) => {
+          try {
+            if (err) throw err
+            console.log(cookies);
+            const browser = await puppeteer.launch({
+              ignoreDefaultArgs: ['--mute-audio'],
+              headless: false,
+              args,
+            });
+
+            const page = await browser.newPage();
+            await page.setViewport({
+              width: 1920,
+              height: 1080
+            });
+            await page.goto(`http://localhost:${localPort}`);
+            const transferCookies = Object.keys(cookies).map(x => ({
+              name: x,
+              value: cookies[x],
+            }))
+
+            await page.setCookie(...transferCookies);
+            await page.reload({
+              waitUntil: ["networkidle0", "domcontentloaded"]
+            });
+          } catch (err) {
+            console.log(err);
+          }
+        });
+    })
+  }
+}
+
+module.exports = AutoFillCookie
