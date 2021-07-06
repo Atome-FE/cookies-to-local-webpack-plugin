80113f4b5e0bb3ff702b79b3ed16c512b9ffd811
baixiaojian <xiaojian.bai@advance.ai>
2021-07-06 10:57:17 +0800

    fix: bug

--- index.js
+++ index.js
@@ -7,38 +7,40 @@ class AutoFillCookie {
   }
 
   apply(compiler) {
-    const { targetUrl, localPort, args = [] } = this.options
-    compiler.hooks.done.tap('AutoFillCookie', () => {
-        chrome.getCookies(targetUrl, async (err, cookies) => {
-          try {
-            if (err) throw err
-            console.log(cookies);
-            const browser = await puppeteer.launch({
-              ignoreDefaultArgs: ['--mute-audio'],
-              headless: false,
-              args,
-            });
+    const { enable, targetUrl, localPort, args = [] } = this.options
+    if (enable) {
+      compiler.hooks.done.tap('AutoFillCookie', () => {
+          chrome.getCookies(targetUrl, async (err, cookies) => {
+            try {
+              if (err) throw err
+              console.log(cookies);
+              const browser = await puppeteer.launch({
+                ignoreDefaultArgs: ['--mute-audio'],
+                headless: false,
+                args,
+              });
 
-            const page = await browser.newPage();
-            await page.setViewport({
-              width: 1920,
-              height: 1080
-            });
-            await page.goto(`http://localhost:${localPort}`);
-            const transferCookies = Object.keys(cookies).map(x => ({
-              name: x,
-              value: cookies[x],
-            }))
+              const page = await browser.newPage();
+              await page.setViewport({
+                width: 1920,
+                height: 1080
+              });
+              await page.goto(`http://localhost:${localPort}`);
+              const transferCookies = Object.keys(cookies).map(x => ({
+                name: x,
+                value: cookies[x],
+              }))
 
-            await page.setCookie(...transferCookies);
-            await page.reload({
-              waitUntil: ["networkidle0", "domcontentloaded"]
-            });
-          } catch (err) {
-            console.log(err);
-          }
-        });
-    })
+              await page.setCookie(...transferCookies);
+              await page.reload({
+                waitUntil: ["networkidle0", "domcontentloaded"]
+              });
+            } catch (err) {
+              console.log(err);
+            }
+          });
+      })
+    }
   }
 }
 
