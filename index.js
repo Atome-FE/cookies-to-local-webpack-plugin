7e3cc5055e84396ceec0aa59f3193174aff9c5a0
baixiaojian <15101661380@163.com>
2021-07-11 12:01:40 +0800

    fix: first run

--- index.js
+++ index.js
@@ -4,41 +4,45 @@ const chrome = require('chrome-cookies-secure');
 class AutoFillCookie {
   constructor(options) {
     this.options = options;
+    this.init = true
   }
 
   apply(compiler) {
     const { enable, targetUrl, localPort, args = [] } = this.options
     compiler.hooks.done.tap('AutoFillCookie', () => {
       if (enable) {
-        chrome.getCookies(targetUrl, async (err, cookies) => {
-          try {
-            if (err) throw err
-            console.log(cookies);
-            const browser = await puppeteer.launch({
-              ignoreDefaultArgs: ['--mute-audio'],
-              headless: false,
-              args,
-            });
+        if (this.init) {
+          this.init = false
+          chrome.getCookies(targetUrl, async (err, cookies) => {
+            try {
+              if (err) throw err
+              console.log(cookies);
+              const browser = await puppeteer.launch({
+                ignoreDefaultArgs: ['--mute-audio'],
+                headless: false,
+                args,
+              });
 
-            const page = await browser.newPage();
-            await page.setViewport({
-              width: 1920,
-              height: 1080
-            });
-            await page.goto(`http://localhost:${localPort}`);
-            const transferCookies = Object.keys(cookies).map(x => ({
-              name: x,
-              value: cookies[x],
-            }))
+              const page = await browser.newPage();
+              await page.setViewport({
+                width: 1920,
+                height: 1080
+              });
+              await page.goto(`http://localhost:${localPort}`);
+              const transferCookies = Object.keys(cookies).map(x => ({
+                name: x,
+                value: cookies[x],
+              }))
 
-            await page.setCookie(...transferCookies);
-            await page.reload({
-              waitUntil: ["networkidle0", "domcontentloaded"]
-            });
-          } catch (err) {
-            console.log(err);
-          }
-        });
+              await page.setCookie(...transferCookies);
+              await page.reload({
+                waitUntil: ["networkidle0", "domcontentloaded"]
+              });
+            } catch (err) {
+              console.log(err);
+            }
+          });
+        }
       }
   })
   }
